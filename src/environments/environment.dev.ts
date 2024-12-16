20b525fc1de2a75f817be0249057025997057273
emilsonsn <emilsonsn2@gmail.com>
2024-12-15 21:33:46 -0300

    setup: configura enviroments do sistema

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,8 @@ export const environment = {
   production: false,
   appName: 'Frontend App',
   home: '/painel',
-  api: 'http://127.0.0.1:8000/api',
+  // api: 'http://127.0.0.1:8000/api',
+  api: 'https://social.guimadureira.com:3001/api',
   version: require('../../package.json').version
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: false,
   appName: 'Frontend App',
   home: '/painel/home',
-  api: 'http://127.0.0.1:8000/api',
+  // api: 'http://127.0.0.1:8000/api',
+  api: 'https://social.guimadureira.com:3001/api',
   version: require('../../package.json').version
 };
